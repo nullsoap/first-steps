d4e82a3a0809c3702a0751fc916c89824ba8f6d4
nullsoap <nullsoap@gmail.com>
2016-06-07 10:35:13 -0700

    This is a basic animation I guess.
    Signed-off-by: nullsoap <nullsoap@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	int scrWidth, scrHeight;
+	scrWidth = 1024;
+	scrHeight = 768;
+	
+	sf::RenderWindow window(sf::VideoMode(scrWidth, scrHeight), "\"Bouncing\" ball");
+	sf::Clock clock;
+	
+	sf::RectangleShape ball(sf::Vector2f(10,10));
+	ball.setFillColor(sf::Color::White);
+	ball.setPosition(100, 100);
+	float speed = 100.f;
+	bool right = true;
 	
 	while (window.isOpen())
 	{
@@ -12,11 +22,25 @@ int main()
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
+			{
+				std::cout << "window closed!" << std::endl;
 				window.close();
+			}
 		}
 		
+		float delta = clock.restart().asSeconds();
+		
+		if (ball.getPosition().x < 0) {
+			right = true;
+		} else if (ball.getPosition().x > scrWidth - ball.getSize().x) {
+			right = false;
+		}
+		
+		if (right) ball.move(speed * delta, 0.f);
+		else ball.move(speed * delta * -1.f, 0.f);
+		
 		window.clear();
-		window.draw(shape);
+		window.draw(ball);
 		window.display();
 	}
 	
